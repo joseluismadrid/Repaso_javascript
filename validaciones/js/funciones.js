9adb531147ca7127fdb0e6f2e51f4dbeca6ed21d
Jose luis <elmueca611201@gmail.com>
2023-10-08 18:43:09 -0500

    Quinto Modificacion

--- validaciones/js/funciones.js
+++ validaciones/js/funciones.js
@@ -1,13 +1,34 @@
 const validarContacto =()=>{
  // validar campos vacios 
- let documentos= document.querySelector('#documento');
- let nombres= document.querySelector('#nombres');
+ let documentos = document.querySelector('#documento');
+ let nombres = document.querySelector('#nombres');
+ let edad = document.querySelectorAll('#edad');
+ let parentesco = document.querySelectorAll('#parentesco');
+ let tipoDocumento = document.querySelectorAll('#tipoDocumento');
+ let estado = document.querySelectorAll('#estado');
+ let telefono = document.querySelectorAll('#telefono');
+ let correo = document.querySelectorAll('#correo');
+ let direccion = document.querySelectorAll('#direccion');
+ let barrio = document.querySelectorAll('#barrio')
+ 
  if (documentos.value.length ==0){
     alert('El documento esta vacio. Validalo')
  }
  else if(nombres.value.length == 0){
     alert('El Nombre esta vacio. Validalo')
  }
+ else if(edad.value.length == 0){
+   alert('La edad esta vacio. Validalo')
+}
+else if(parentesco.value.length == 0){
+   alert('El parentesco esta vacio. Validalo')
+}
+else if(tipoDocumento.value == 0 ){
+   alert('No se a seleccionado una opccion. Validalo')
+}
+else if(estado.value == 0 ){
+   alert('No se a seleccionado una opccion. Validalo')
+}
  else{
     alert('Datos enviados correctamente ')
  }
