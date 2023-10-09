beefd346749a0f53f93028c3ccad4f97293bc286
Jose luis <elmueca611201@gmail.com>
2023-10-08 21:52:59 -0500

    Novena modificacion

--- validaciones/js/funciones.js
+++ validaciones/js/funciones.js
@@ -1,42 +1,59 @@
-const validarContacto =()=>{
- // validar campos vacios 
- let documentos = document.querySelector('#documento');
- let nombres = document.querySelector('#nombres');
- let edad = document.querySelectorAll('#edad');
- let parentesco = document.querySelectorAll('#parentesco');
- let tipoDocumento = document.querySelectorAll('#tipoDocumento');
- let estado = document.querySelectorAll('#estado');
- let telefono = document.querySelectorAll('#telefono');
- let correo = document.querySelectorAll('#correo');
- let direccion = document.querySelectorAll('#direccion');
- let barrio = document.querySelectorAll('#barrio')
- 
- if(tipoDocumento.value.length == 0 ){
-   alert('No se a seleccionado una opccion. Validalo')
-}
- else if(nombres.value.length == 0){
-    alert('El Nombre esta vacio. Validalo')
- }
- else if(edad.value.length == 0){
-   alert('La edad esta vacio. Validalo')
-}
-else if(parentesco.value.length == 0){
-   alert('El parentesco esta vacio. Validalo')
-}
-else  if (documentos.value.length ==0){
-   alert('El documento esta vacio. Validalo')
-}
-else if(estado.value.length == 0 ){
-   alert('No se a seleccionado una opccion. Validalo')
-}
- else{
-    alert('Datos enviados correctamente ')
- }
-}
-const boton = document.querySelector('#btnEnviar')
-// Escuchar eventos del boton
-boton.addEventListener('click',validarContacto)
+document.getElementById('btnEnviar').addEventListener('click', function(event) {
+ event.preventDefault();
 
+ var tipoDocumento = document.getElementById('tipoDocumento').value;
+ var documento = document.getElementById('documento').value;
+ var nombres = document.getElementById('nombres').value;
+ var edad = document.getElementById('edad').value;
+ var parentesco = document.getElementById('parentesco').value;
+ var estado = document.getElementById('estado').value;
+ var telefono = document.getElementById('telefono').value;
+ var correo = document.getElementById('correo').value;
+ var direccion = document.getElementById('direccion').value;
+ var barrio = document.getElementById('barrio').value;
 
-// diseña uno de los formularios asignados en su proyecto 
-// validadr campos obligatorios con js  subir de git
+ if (tipoDocumento === '0') {
+    alert('Por favor, seleccione un tipo de documento.');
+    return;
+ }
+
+ if (documento === '') {
+    alert('Por favor, ingrese el número de documento.');
+    return;
+ }
+
+ if (nombres === '') {
+    alert('Por favor, ingrese el nombre completo.');
+    return;
+ }
+
+ if (edad === '') {
+    alert('Por favor, ingrese la edad.');
+    return;
+ }
+
+ if (parentesco === '') {
+    alert('Por favor, ingrese el parentesco.');
+    return;
+ }
+
+ if (estado === '') {
+    alert('Por favor, seleccione un estado.');
+    return;
+ }
+
+ if (telefono === '') {
+    alert('Por favor, ingrese el número de teléfono.');
+    return;
+ }
+
+ if (correo === '') {
+    alert('Por favor, ingrese el correo electrónico.');
+    return;
+ }
+
+ if (direccion === '') {
+      alert('Por favor, ingrese la dirección.');
+      return;
+   }
+});
