e8cafb25351a0022cbfbd2f957190bdcaa015d50
joseluismadrid <joseluis6112011@hotmail.com>
2023-10-10 14:18:07 -0500

    tema nuevo

--- promesas/js/funciones.js
+++ promesas/js/funciones.js
@@ -1,18 +1,32 @@
 const buscarCliente = (dato)=>{
-    const documentos = [1010,1011,1020,1030,1957,5550]
-    let encontrado =false
-    for (let i = 0; i < documentos.length; i++){
-        if (dato == documentos[i]){
-            encontrado=true
-        }
+    const documentos = [1010, 1011, 1020, 1030, 1957, 5550];
+    const nombres = ['carmen', 'ines', 'juan', 'Daniela', 'Maria', 'janeth'];
+    // Agregar el codigo que devuelva el la promesa el nombre solo si
+    // el documento existe en javascript;
+    const inicio = documentos.indexOf(parseInt(dato));
+    let encontrado = false;
+    for (let i = 0; i < documentos.length; i++) {
+      if (dato == documentos[i]) {
+        encontrado = true;
+      }
     }
-  return new Promise((resolve,reject)=>{
-    if (encontrado){
-        resolve('Dato Encontrado')
-    }else{
-        reject('Dato No Encontrado')
-    }
-  });
-}
+    return new Promise((resolve, reject) => {
+      if (inicio !=-1) {
+        const nombre=nombres[inicio];
+        resolve(nombre);
+      } else {
+        reject('Dato No Encontrado');
+      }
+    });
+};
+
 document.querySelector('#btnBuscar')
-.addEventListener('click',()=>buscarCliente(document.getElementById('dato').value))
+.addEventListener('click',
+()=>buscarCliente(document.getElementById('dato').value)
+.then(mensaje=>console.log(mensaje))
+.catch(error=>console.log(error))
+)
+
+// Validar el login del proyecto formativo con una promesa 
+// Emplear datos constantes
+// dilopezz@sena.edu.co
