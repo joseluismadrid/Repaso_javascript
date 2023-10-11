5553d2e1f5d7485208431bd3b23d05069c6b60cf
joseluismadrid <joseluis6112011@hotmail.com>
2023-10-11 14:12:35 -0500

    Primer Cambio

--- sincronica/js/funciones.js
+++ sincronica/js/funciones.js
@@ -1,6 +1,6 @@
 const proceso1 = () => {
     let suma1 =0;    
-        for (let i = 0; i<= 5000; i++){
+        for (let i = 0; i<= 50; i++){
             suma1 += i
         }
         return(` Este es el proceso 1 y la suma es ${suma1}`)
@@ -9,7 +9,7 @@ const proceso1 = () => {
  }
  const proceso2 = () => {
     let suma2 =0;   
-        for (let i = 0; i<= 20000; i++){
+        for (let i = 0; i<= 20; i++){
             suma2 += i
         }
         return(` Este es el proceso 2 y la suma es ${suma2}`)
@@ -19,7 +19,7 @@ const proceso1 = () => {
  const proceso3 = () => {
     let suma3 =0;
     
-        for (let i = 0; i<= 1000; i++){
+        for (let i = 0; i<= 10; i++){
             suma3 += i
         }
        return (` Este es el proceso 3 y la suma es ${suma3}`)
